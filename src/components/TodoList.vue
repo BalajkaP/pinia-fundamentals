c559e46b688e419b259ecd1ba3cd8c7668300472
BalajkaP <p.b.1@seznam.cz>
2025-09-04 21:03:40 +0200

    koments

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { useTodoListStore } from '@/stores/todoList'
-import { storeToRefs } from 'pinia'
+import { useTodoListStore } from "@/stores/todoList"
+import { storeToRefs } from "pinia"
 
 const store = useTodoListStore()
 
@@ -9,16 +9,23 @@ const { todoList } = storeToRefs(store)
 
 // destructuring action method doesn't require using storeToRefs:
 const { toggleCompleted, deleteTodo } = store
-
 </script>
 
 <template>
-  <div v-for="todo in todoList" :key="todo.id" class="item">
+  <div
+    v-for="todo in todoList"
+    :key="todo.id"
+    class="item"
+  >
     <div class="content">
       <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
       <div>
         <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
-        <span @click="deleteTodo(todo.id)" class="x">&#10060;</span>
+        <span
+          @click="deleteTodo(todo.id)"
+          class="x"
+          >&#10060;</span
+        >
       </div>
     </div>
   </div>
